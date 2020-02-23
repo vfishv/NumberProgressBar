e84cffe3ec7ea7c00835eb48c5c632eaeaa9a7b5
Quentin <vFISHv@gmail.com>
2020-02-24 00:48:48 +0800

    build

--- demo/src/main/java/com/daimajia/numberprogressbar/example/MainActivity.java
+++ demo/src/main/java/com/daimajia/numberprogressbar/example/MainActivity.java
@@ -1,7 +1,7 @@
 package com.daimajia.numberprogressbar.example;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -13,7 +13,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 
-public class MainActivity extends ActionBarActivity implements OnProgressBarListener {
+public class MainActivity extends AppCompatActivity implements OnProgressBarListener {
     private Timer timer;
 
     private NumberProgressBar bnp;
